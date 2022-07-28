940e1a7483777165270e26c15b63bcdc7b299cd6
段舒文 <107804840+dsw676676@users.noreply.github.com>
2022-07-28 10:19:29 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
 print("hello world!")
+def mad(x):
+    if x>=0.7:
+        return "x｛｝ is big".format(x)
+    else:
+        return "x｛｝ is small".format(x)
+
+if __name__ == '__main__':
+    print(mad(random.random())
